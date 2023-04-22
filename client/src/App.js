c6d9a1bfc99912bacb15b5ddd4c9cc4be6ad8c53
Jason Ling <105073210+Jasonlingg@users.noreply.github.com>
2023-04-22 12:59:16 -0400

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+
+function App() {
+  const navigate = useNavigate();
+  return (
+    <div className="App">
+      <h1>Home page</h1>
+      <Button onClick={() => navigate("create")}>NEXT</Button>
+    </div>
+  );
+}
+
+export default App;
